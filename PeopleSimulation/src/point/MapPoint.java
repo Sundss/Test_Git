36133a142e8d2a67bde0427937f1f8025437687d
sundeshan <sundeshan1@gmail.com>
2015-05-19 18:09:00 +0800

    test2

---
+++ PeopleSimulation/src/point/MapPoint.java
@@ -0,0 +1,26 @@
+package point;
+
+public class MapPoint {
+
+	private int x;
+	private int y;
+	
+	public MapPoint(int x,int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+}
